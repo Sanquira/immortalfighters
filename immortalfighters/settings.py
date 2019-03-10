2f9c73e7c806211248c95eb2c57c6b79c9d5e3b4
Sanquira Van Delbar <sanquira.van.delbar@gmail.com>
2019-03-10 12:13:38 +0100

    Add missing packages to install. Change of language to cs.

--- immortalfighters/settings.py
+++ immortalfighters/settings.py
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework_swagger',
-    'bootstrap3',
+    'bootstrap4',
     'sass_processor',
     'base',
     'guilds',
@@ -113,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'cs'
 
 TIME_ZONE = 'UTC'
 
