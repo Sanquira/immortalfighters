62b70a6f98937e859ac2ea30e2f872e1b2fcd4db
sanquira <jimmy.hilton@seznam.cz>
2019-04-20 19:06:47 +0200

    Renaming things
    Add verbose for Spell, ProfessionLimitation

--- dictionary/models/profession_limitation.py
+++ dictionary/models/profession_limitation.py
@@ -1,10 +1,16 @@
 from django.db import models
 
-from dictionary.models import Spell
 from dictionary.models.profession import BaseProfession
 
 
 class ProfessionLimitation(models.Model):
-    skill = models.ForeignKey(Spell, on_delete=models.CASCADE)
+    skill = models.ForeignKey('Spell', on_delete=models.CASCADE)
     profession = models.ForeignKey(BaseProfession, on_delete=models.CASCADE)
     from_level = models.PositiveSmallIntegerField(default=1)
+
+    def __str__(self):
+        return self.profession.name + " - " + self.skill.name
+
+    class Meta:
+        verbose_name = "Povolání - kouzlo omezení"
+        verbose_name_plural = verbose_name
--- dictionary/models/spell.py
+++ dictionary/models/spell.py
@@ -11,4 +11,11 @@ class Spell(models.Model):
     duration = models.CharField(max_length=32, null=True)
     note = models.TextField(null=True)
 
-    available_for_proffesions = models.ManyToManyField(BaseProfession, through='ProfessionLimitation')
+    available_for_professions = models.ManyToManyField(BaseProfession, through='ProfessionLimitation')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Kouzlo"
+        verbose_name_plural = "Kouzla"
