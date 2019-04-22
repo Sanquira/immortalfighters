560c377ba8930197e63e008d140617d37ef6458a
sanquira <jimmy.hilton@seznam.cz>
2019-04-22 21:34:52 +0200

    Add verbose for IFUser and Character

--- base/models/character.py
+++ base/models/character.py
@@ -5,15 +5,19 @@ from dictionary.models import Race, BaseProfession
 
 
 class Character(models.Model):
-    owner = models.ForeignKey(IFUser, on_delete=models.CASCADE)
-    character_name = models.CharField(max_length=50, null=True)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE, null=True)
-    profession = models.ForeignKey(BaseProfession, on_delete=models.CASCADE, null=True)
+    owner = models.ForeignKey(IFUser, on_delete=models.CASCADE, verbose_name="Vlastník")
+    character_name = models.CharField(max_length=50, null=True, verbose_name="Jméno postavy")
+    race = models.ForeignKey(Race, on_delete=models.CASCADE, null=True, verbose_name="Rasa")
+    profession = models.ForeignKey(BaseProfession, on_delete=models.CASCADE, null=True, verbose_name="Povolání")
 
-    stat_strength = models.PositiveSmallIntegerField(default=0)
-    stat_dexterity = models.PositiveSmallIntegerField(default=0)
-    stat_resistance = models.PositiveSmallIntegerField(default=0)
-    stat_intelligence = models.PositiveSmallIntegerField(default=0)
-    stat_charisma = models.PositiveSmallIntegerField(default=0)
-    experience_points = models.PositiveIntegerField(default=0)
-    level = models.PositiveSmallIntegerField(default=0)
+    # stat_strength = models.PositiveSmallIntegerField(default=0)
+    # stat_dexterity = models.PositiveSmallIntegerField(default=0)
+    # stat_resistance = models.PositiveSmallIntegerField(default=0)
+    # stat_intelligence = models.PositiveSmallIntegerField(default=0)
+    # stat_charisma = models.PositiveSmallIntegerField(default=0)
+    # experience_points = models.PositiveIntegerField(default=0)
+    # level = models.PositiveSmallIntegerField(default=0)
+
+    class Meta:
+        verbose_name = _("Postava")
+        verbose_name_plural = _("Postavy")
--- base/models/ifuser.py
+++ base/models/ifuser.py
@@ -3,5 +3,8 @@ from django.db import models
 
 
 class IFUser(AbstractUser, models.Model):
-    active_char = models.ForeignKey("Character", on_delete=models.CASCADE, null=True)
-    pass
+    active_char = models.ForeignKey("Character", on_delete=models.CASCADE, null=True, verbose_name="Aktivní postava")
+
+    class Meta:
+        verbose_name = _("Uživatel")
+        verbose_name_plural = _("Uživatelé")
