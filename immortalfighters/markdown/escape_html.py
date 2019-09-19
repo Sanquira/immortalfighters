997157794e10c07caf1c725e53f71d7a300e844f
TheSprtCz <halapetr@selfnet.cz>
2019-09-19 19:59:20 +0200

    Escape HTML in Markdown

---
+++ immortalfighters/markdown/escape_html.py
@@ -0,0 +1,7 @@
+from markdown.extensions import Extension
+
+
+class EscapeHtml(Extension):
+    def extendMarkdown(self, md):
+        md.preprocessors.deregister('html_block')
+        md.inlinePatterns.deregister('html')
