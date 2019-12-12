10abfd4dec53e373eac2eb678d18e9a2471f22a3
TheSprtCz <halapetr@selfnet.cz>
2019-12-12 20:02:00 +0100

    Fix permissions pylint errors
    - Document the whole Permission submodule

--- permissions/apps.py
+++ permissions/apps.py
@@ -1,5 +1,9 @@
+"""
+Contains application configuration for django
+"""
 from django.apps import AppConfig
 
 
 class PermissionsConfig(AppConfig):
+    """Configuration of the application"""
     name = 'permissions'
--- permissions/utils.py
+++ permissions/utils.py
@@ -1,37 +1,41 @@
+"""
+Utility classes for the Permission submodule
+"""
 import abc
 from abc import ABC
 
 
 class AbstractPermissionSystem(ABC):
+    """Interface for every PermissionSystem"""
 
     @abc.abstractmethod
     def list_groups(self):
-        pass
+        """Lists all the groups that exist in this manager"""
 
     @abc.abstractmethod
     def has_group(self, group_name: str) -> bool:
-        pass
+        """Checks if group exists"""
 
     @abc.abstractmethod
     def get_group_by_name(self, group_name: str):
-        pass
+        """Finds group by name"""
 
     @abc.abstractmethod
     def create_group(self, group_name: str):
-        pass
+        """Creates new group, if the group exists, returns the existing one"""
 
     @abc.abstractmethod
     def has_permission(self, subject, permission: str) -> bool:
-        pass
+        """Finds permission object for subject by the permission name"""
 
     @abc.abstractmethod
     def list_permissions(self, subject):
-        pass
+        """Lists all the permissions"""
 
     @abc.abstractmethod
     def add_permission(self, subject, permission: str):
-        pass
+        """Adds new permission for the subject"""
 
     @abc.abstractmethod
     def remove_permission(self, subject, permission: str):
-        pass
+        """Removes the permission from the subject"""
